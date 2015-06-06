7253eca5bbbc4b776cf5ea991edf1066c956136f
ACollectionOfAtoms <hernandezdavid88@utexas.edu>
2015-06-06 11:35:38 -0500

    Simple bar chart

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-var body = d3.select("body");
-var p = body.append("p");
-p.text("New paragraph!");
+var dataset = [ 5, 10, 15, 20, 25 ];
+
+d3.select("body").selectAll("div")
+	.data(dataset)
+	.enter()
+	.append("div")
+	.attr("class", "bar")
+	.style("height", function(d) {
+		return d + "px";
+	});
